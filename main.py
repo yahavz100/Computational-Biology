16157368cb5bb9c43d946703af989d48e50dc137
Yahav <yahavz100@gmail.com>
2023-04-02 23:28:15 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Yahav Zarfati
+# Noa Miara Levi
+
+class Person:
+    def __init__(self, x, y, level_of_skepticism=0):
+        self.x = x
+        self.y = y
+        self.level_of_skepticism = level_of_skepticism
+
+
+if __name__ == '__main__':
+    print("hello world")
