612236715276b57c758b317002854a64e41cdb22
noa miara <noamiara1998@outlook.co.il>
2023-04-03 09:32:36 +0300

    init_persons and other function

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import random
 from matplotlib import pyplot as plt
 from matplotlib import colors as c
 import numpy as np
+from typing import List
 
-SIZE = 100
 global P
+SIZE = 100
+L = 10
 
 # Define the probability of passing on the rumor for each level of skepticism
 P_S1 = 1.0
@@ -18,14 +20,20 @@ s2_ratio = 0.1
 s3_ratio = 0.3
 s4_ratio = 0.4
 
+
 class Person:
-    def __init__(self, x, y, level_of_skepticism=0):
+    def __init__(self, x, y, level_of_skepticism=0, generations_left=L):
         self.x = x
         self.y = y
         self.level_of_skepticism = level_of_skepticism
+        self.generations_left = generations_left
 
 
-# init the list of persons and the grid accordingly
+"""
+init the list of persons and the grid accordingly
+"""
+
+
 def init_persons():
     # Define skepticism levels and their corresponding ratios
     skepticism_levels = [1, 2, 3, 4]
@@ -44,6 +52,34 @@ def init_persons():
                 persons.append(person)
 
     return persons, grid
+
+
+"""
+Person decide if to accept rumor based on probability,
+"""
+
+
+def accept_rumor(p: float) -> bool:
+    pass
+
+
+"""
+Draw cached matrix to client
+"""
+
+
+def draw_to_client(matrix: np.ndarray, people: List[Person]):
+    pass
+
+
+"""
+Function receive a person and a list of its neighbors, 
+decides according to his neighbors the person temp level of skepticism
+"""
+
+
+def take_decision(person: Person, neighbors: List[Person]):
+    pass
 
 
 if __name__ == '__main__':
