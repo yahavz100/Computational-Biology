41563ef5344f0e0ba41ce087fcb0c2aea08fe7d3
Yahav <yahavz100@gmail.com>
2023-04-10 18:43:53 +0300

    Added documenting

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 # Yahav Zarfati
 # Noa Miara Levi
-import random
 
+import random
+from typing import List, Tuple
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import colors as c
 
-P = 0.5
-SIZE = 5
-L = 10
+P: float = 0.5
+SIZE: int = 5
+L: int = 10
 
 # Define the probability of passing on the rumor for each level of skepticism
-P_S2 = 0.67
-P_S3 = 0.33
+P_S2: float = 0.67
+P_S3: float = 0.33
 
-S1 = 1
-S2 = 2
-S3 = 3
-S4 = 4
+S1: int = 1
+S2: int = 2
+S3: int = 3
+S4: int = 4
 
-s2_ratio = 0.2
-s3_ratio = 0.1
-s4_ratio = 0.1
+s2_ratio: float = 0.2
+s3_ratio: float = 0.1
+s4_ratio: float = 0.1
 
 
-def init_grid():
+def init_grid() -> Tuple[np.ndarray, List['Person']]:
+    """
+    Initialize a grid with persons randomly distributed across it and return the grid and persons.
+    Returns:
+    A tuple containing the initialized grid and a list of Person instances.
+    """
     # Define skepticism levels and their corresponding ratios
     skepticism_levels = [1, 2, 3, 4]
     skepticism_ratios = [1 - s2_ratio - s3_ratio - s4_ratio, s2_ratio, s3_ratio, s4_ratio]
@@ -46,16 +52,22 @@ def init_grid():
     return grid, persons
 
 
-def copy_grid_by_rumors_recieved(grid):
+def copy_grid_by_rumors_received(grid: np.ndarray) -> list[list[int]]:
+    """
+    Return a copy of the input grid where each person is represented by the rumor_received attribute.
+    """
     return [[0 if person is None else person.rumor_received for person in row] for row in grid]
 
 
-def display_grid(grid: np.ndarray):
+def display_grid(grid: np.ndarray) -> None:
+    """
+    Display the grid as an image.
+    """
     fig, ax = plt.subplots()
     cmap = c.ListedColormap(['white', 'yellow', 'red'])
     bounds = [0, 1, 2, 3]
     norm = c.BoundaryNorm(bounds, cmap.N)
-    grid_to_show = copy_grid_by_rumors_recieved(grid)
+    grid_to_show = copy_grid_by_rumors_received(grid)
     ax.imshow(grid_to_show, cmap=cmap, norm=norm)
     plt.show()
     # plt.draw()
@@ -64,7 +76,10 @@ def display_grid(grid: np.ndarray):
     # plt.cla()
 
 
-def main_loop(grid, persons):
+def main_loop(grid: np.ndarray, persons: list) -> None:
+    """
+    Simulate the spreading of a rumor throughout the grid.
+    """
     random_person: Person = random.choice(persons)
     queue = [random_person]
     is_first_person = True
@@ -97,7 +112,10 @@ class Person:
         self.generations_left = 0
         self.rumor_received = False
 
-    def scan_neighbors(self, grid):
+    def scan_neighbors(self, grid: np.ndarray) -> list:
+        """
+        Scans the neighbors of the current person and returns a list of neighbors.
+        """
         neighbors = []
         x = self.x
         y = self.y
@@ -110,6 +128,9 @@ class Person:
         return neighbors
 
     def decide_if_to_accept_rumor(self):
+        """
+        Decides whether the person should accept the rumor or not.
+        """
         if self.level_of_skepticism == S1:
             self.rumor_received = True
             self.generations_left = L
