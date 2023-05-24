ed85649ddddfc63e3430c16e740ea5374b40f5cd
Yahav <yahavz100@gmail.com>
2023-05-24 20:59:17 +0300

    added comments

--- Ex2/main.py
+++ Ex2/main.py
@@ -47,6 +47,15 @@ def load_english_words(filename: str) -> set:
 def optimize_key_fitness(encrypted_text, given_letter_freq, given_letter_pairs_freq, words, fitness_counter,
                          mutation_rate=0.05, num_generations=100, population_size=50):
     def calculate_fitness(fitness_counter, given_letter_freq, given_letter_pairs_freq, english_words, plain_text):
+        """
+        Calculate the fitness score of a decryption key.
+        :param fitness_counter: Counter for the fitness function.
+        :param given_letter_freq: Frequency of letters in the decrypted text.
+        :param given_letter_pairs_freq: Frequency of letter pairs in the decrypted text.
+        :param english_words: Set of English words.
+        :param plain_text: Decrypted text.
+        :return: Fitness score of the decryption key.
+        """
         fitness_counter += 1
         fitness = 0.0
         fitness += sum(1.0 for word in plain_text.lower().split() if word in english_words)
@@ -55,15 +64,28 @@ def optimize_key_fitness(encrypted_text, given_letter_freq, given_letter_pairs_f
         return fitness
 
     def mapping_pmx(key1, key2, a, b, child_b_sequence, crossover_point1, crossover_point2):
+        """
+        Perform Partially Mapped Crossover (PMX) for key sequences.
+        :param key1: Parent key 1.
+        :param key2: Parent key 2.
+        :param a: Key sequence of parent 1.
+        :param b: Key sequence of parent 2.
+        :param child_b_sequence: Child key sequence.
+        :param crossover_point1: First crossover point.
+        :param crossover_point2: Second crossover point.
+        :return: Child key sequences after PMX.
+        """
         child_a_seq = a
         mapping_a = {key1.sequence[i]: b[i] for i in range(crossover_point1, crossover_point2 + 1)}
         mapping_b = {key2.sequence[i]: child_a_seq[i] for i in range(crossover_point1, crossover_point2 + 1)}
+
         for i in range(len(key2.sequence)):
             if i < crossover_point1 or i > crossover_point2:
                 value = key2.sequence[i]
                 while value in mapping_a:
                     value = mapping_a[value]
                 child_a_seq[i] = value
+
         for i in range(len(key1.sequence)):
             if i < crossover_point1 or i > crossover_point2:
                 value = key1.sequence[i]
@@ -73,29 +95,62 @@ def optimize_key_fitness(encrypted_text, given_letter_freq, given_letter_pairs_f
         return child_a_seq, child_b_sequence
 
     def crossover(key1, key2):
+        """
+        Perform crossover between two decryption keys.
+        :param key1: First parent key.
+        :param key2: Second parent key.
+        :return: Two child keys generated through crossover.
+        """
+
+        # Select random crossover points
         crossover_point1 = np.random.randint(1, len(key1.sequence))
         crossover_point2 = np.random.randint(crossover_point1, len(key1.sequence))
+
+        # Create copies of key1 and key2
         child_a, child_b = key1.deepcopy(), key2.deepcopy()
+
+        # Create temporary arrays to hold the crossover segments
         a, b = np.empty_like(key1.sequence), np.empty_like(key2.sequence)
+
+        # Perform crossover by exchanging the segments between the parents
         a[crossover_point1:crossover_point2 + 1], b[crossover_point1:crossover_point2 + 1] = \
             key1.sequence[crossover_point1:crossover_point2 + 1], key2.sequence[crossover_point1:crossover_point2 + 1]
+
+        # Apply position-based mapping crossover (pmx) to generate child sequences
         child_a.sequence, child_b.sequence = mapping_pmx(key1, key2, a, b, child_b.sequence, crossover_point1,
                                                          crossover_point2)
+
         return child_a, child_b
 
     def mutation_function(child_1, child_2, mutation_rate):
+        """
+        Perform mutation on two child keys.
+        :param child_1: First child key.
+        :param child_2: Second child key.
+        :param mutation_rate: Rate of mutation.
+        :return: Mutated child keys.
+        """
+
+        # Create copies of the children
         new_key_child_1, new_key_child_2 = child_1.deepcopy(), child_2.deepcopy()
+
+        # Select random letters for mutation
         letter1, letter2 = np.random.randint(len(child_1.sequence)), np.random.randint(len(child_2.sequence))
+
+        # Mutate child 1
         if np.argwhere(np.random.rand(*child_1.sequence.shape) <= mutation_rate).size:
             for i in np.argwhere(np.random.rand(*child_1.sequence.shape) <= mutation_rate):
+                # Swap the letters at the mutation positions
                 new_key_child_1.sequence[i.item()], new_key_child_1.sequence[letter1] = \
-                    new_key_child_1.sequence[letter1], new_key_child_1.sequence[
-                        i.item()]
+                    new_key_child_1.sequence[letter1], new_key_child_1.sequence[i.item()]
+
+        # Mutate child 2
         if np.argwhere(np.random.rand(*child_2.sequence.shape) <= mutation_rate).size:
             for i in np.argwhere(np.random.rand(*child_2.sequence.shape) <= mutation_rate):
+                # Swap the letters at the mutation positions
                 new_key_child_2.sequence[i.item()], new_key_child_2.sequence[letter2] = \
-                    new_key_child_2.sequence[letter2], new_key_child_2.sequence[
-                        i.item()]
+                    new_key_child_2.sequence[letter2], new_key_child_2.sequence[i.item()]
+
         return new_key_child_1, new_key_child_2
 
     def roulette_wheel_selection(p):
@@ -113,60 +168,111 @@ def optimize_key_fitness(encrypted_text, given_letter_freq, given_letter_pairs_f
         return selected_index
 
     def decrypt_text(code):
-        code_dict = dict(zip(np.array(list('abcdefghijklmnopqrstuvwxyz')), code))
+        """
+        Decrypt the encrypted text using a given code.
+
+        :param code: List representing the decryption code.
+        :return: Decrypted text.
+        """
+
+        # Create a dictionary mapping English letters to the code letters
+        code_dict = dict(zip(list('abcdefghijklmnopqrstuvwxyz'), code))
+
+        # Create a mapping dictionary with default values as the original letters
         mapp = {letter: code_dict.get(letter, letter) for letter in list('abcdefghijklmnopqrstuvwxyz')}
-        return encrypted_text.translate(str.maketrans(mapp))
+
+        # Translate the encrypted text using the mapping dictionary
+        decrypted_text = encrypted_text.translate(str.maketrans(mapp))
+
+        return decrypted_text
 
     empty_individual = structure()
     empty_individual.sequence = None
     empty_individual.fitness = None
+
+    # Initialize the best_key with an empty individual
     best_key = empty_individual.deepcopy()
     best_key.fitness = -np.inf
+
+    # Create a breeding population of empty individuals
     breeding_population = empty_individual.repeat(population_size)
+
+    # Generate initial population and calculate their fitness scores
     for i in range(population_size):
+        # Generate a random permutation sequence as the individual's sequence
         breeding_population[i].sequence = np.random.permutation(np.array(list('abcdefghijklmnopqrstuvwxyz')).copy())
+
+        # Decrypt the text using the individual's sequence
         plain_text = decrypt_text(breeding_population[i].sequence)
+
+        # Calculate the fitness of the individual
         breeding_population[i].fitness = calculate_fitness(fitness_counter, given_letter_freq, given_letter_pairs_freq,
-                                                           words,
-                                                           plain_text)
+                                                           words, plain_text)
+
+        # Update the best_key if the individual has higher fitness
         if breeding_population[i].fitness > best_key.fitness:
             best_key = breeding_population[i].deepcopy()
+
+    # Initialize arrays to store fitness scores and average costs for each generation
     fitness_scores = np.empty(num_generations)
     avg = np.empty(num_generations)
 
+    # Perform the evolution process for the specified number of generations
     for Generation in range(num_generations):
         costs = np.array([x.fitness for x in breeding_population])
         average_cost = np.mean(costs)
+
+        # Normalize costs to calculate selection probabilities
         normalized_costs = costs / average_cost if average_cost != 0 else costs
         probs = np.exp(-normalized_costs)
         probs /= np.sum(probs)
-        avg[Generation] = average_cost  # assuming 'Generation' is a variable storing the current generation number
+
+        avg[Generation] = average_cost
+
         print(f"Generation : {Generation}")
 
         population = []
         range_population_size = int(np.round(population_size) * 2) // 2
+
+        # Generate new individuals through crossover and mutation
         for _ in range(range_population_size):
+            # Perform roulette wheel selection to choose parents
             parent1, parent2 = breeding_population[roulette_wheel_selection(probs)], breeding_population[
                 roulette_wheel_selection(probs)]
+
+            # Perform crossover to create two children
             child1, child2 = crossover(parent1, parent2)
+
+            # Perform mutation on the children
             child1, child2 = mutation_function(child1, child2, mutation_rate)
+
+            # Decrypt the text using the children's sequences and calculate their fitness
             plain_text = decrypt_text(child1.sequence)
             child1.fitness = calculate_fitness(fitness_counter, given_letter_freq, given_letter_pairs_freq, words,
                                                plain_text)
+
             plain_text = decrypt_text(child2.sequence)
             child2.fitness = calculate_fitness(fitness_counter, given_letter_freq, given_letter_pairs_freq, words,
                                                plain_text)
+
             population.append(child1)
             population.append(child2)
+
+            # Update the best_key if any of the children have higher fitness
             if child1.fitness > best_key.fitness:
                 best_key = child1.deepcopy()
             elif child2.fitness > best_key.fitness:
                 best_key = child2.deepcopy()
+
+        # Add the new population to the breeding population and select the fittest individuals
         breeding_population += population
         breeding_population = sorted(breeding_population, key=lambda x: x.fitness, reverse=True)
         breeding_population = breeding_population[:population_size]
+
+        # Store the fitness score of the best individual in each generation
         fitness_scores[Generation] = best_key.fitness
-    # print(best_key.sequence)
+
+    # Return the best decryption key, fitness scores, and average costs
     return best_key.sequence, fitness_scores, avg
 
 
